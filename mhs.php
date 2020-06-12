7ac9814d7a4c58291afa315236309945a8edf02e
tyasusantii <tia1800016130@webmail.uad.ac.id>
2020-06-12 22:41:12 +0700

    first commit

---
+++ mhs.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+
+
+<b><center>TIA SUSANTI 1800016130</center></b>
+<BR> <BR>
+<b><center>TEKWEB LANJUT C </center></b>
+<b><center>PRAKTIKUM TEKWEB C</center>
+<BR>
+<BR>
+
+
+<table border="1" align="center">
+<tr>
+     <td width="117">NIM</td>
+     <td width="14">:</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>JENIS KELAMIN</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>ALAMAT</td>
+     <td>:</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td>PROGRAM STUDI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>
+     
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="formmhs.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
---
+++ output.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Output</title>
+</head>
+
+<body>
+<b><center>TIA SUSANTI 1800016130</center></b>
+<BR> 
+<b><center>TEKWEB LANJUT KELAS C </center></b>
+<BR>
+<b><center>PRAKTIKUM TEKWEB C</center>
+<BR>
+<BR>
+
+
+<table border="1" align="center">
+<tr>
+     <td width="117">NIM</td>
+     <td width="14">:</td>
+     <td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>Jenis Kelamin</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>Program Studi</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="mhs.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
